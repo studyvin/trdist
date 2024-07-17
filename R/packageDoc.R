8080322369254b489d38766edb4f377f3022147a
Jared Studyvin <jaredstudyvin@jareds-mac-mini.uwyo.edu>
2024-07-17 10:15:58 -0600

    sending to CRAN

--- R/packageDoc.R
+++ R/packageDoc.R
@@ -5,7 +5,7 @@
 #'
 #' @description Truncation of univariate probability distributions. The probability distribution can come from other packages so long as the function names follow the standard d, p, q, r naming format. Also other univariate probability distributions are included. 
 #' 
-#' @name tdist
+#' @name trdist
 #'
 #' @keywords package
 #'
