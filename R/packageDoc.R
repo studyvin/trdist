7475d30b40883d2d81e8e31d00c32bccd90a0e24
Jared Studyvin <jaredstudyvin@jareds-mac-mini.uwyo.edu>
2024-07-17 11:19:17 -0600

    package citation on attach

--- R/packageDoc.R
+++ R/packageDoc.R
@@ -1,7 +1,7 @@
 
 
 #'
-#' @title Univariate Proability Distribution With Truncation
+#' @title Univariate Proability Distributions with Truncation
 #'
 #' @description Truncation of univariate probability distributions. The probability distribution can come from other packages so long as the function names follow the standard d, p, q, r naming format. Also other univariate probability distributions are included. 
 #' 
@@ -10,3 +10,14 @@
 #' @keywords package
 #'
 "_PACKAGE"
+
+
+.onAttach <- 
+    function(libname, pkgname) {
+        pkName <- 'trdist'
+        packageStartupMessage("\nThank you for using the ",pkName," package!")
+        packageStartupMessage("To acknowledge our work, please cite the package.")
+        packageStartupMessage("Use citation('", pkName,
+                              "') to obtain the citation information. \n")
+        
+}
